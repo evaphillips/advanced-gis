73ac148732a2ab4b38454e8454d60fc74f7306cd
Eva Phillips <60586613+evaphillips@users.noreply.github.com>
2024-04-01 15:20:52 -0400

    add comments

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
+
     $('.image-wrapper').hover(function () {
+        // This function is triggered when the mouse pointer enters the area of an element with the class 'image-wrapper'
+
         $(this).find('img').stop().animate({
-            opacity: 0.2
-        }, 200);
+            // Finds the 'img' element inside the current 'image-wrapper' element and animates its CSS properties
+            opacity: 0.2 // Animates the opacity of the image to 0.2 (20% opacity)
+        }, 200); // Duration of the animation in milliseconds (200ms)
+
         $(this).find('.text').removeClass('hide');
+        // Finds the element with the class 'text' inside the current 'image-wrapper' element and removes the class 'hide'
+
     }, function () {
+        // This function is triggered when the mouse pointer leaves the area of an element with the class 'image-wrapper'
+
         $(this).find('img').stop().animate({
-            opacity: 1
-        }, 500);
+            // Finds the 'img' element inside the current 'image-wrapper' element and animates its CSS properties
+            opacity: 1 // Animates the opacity of the image to 1 (fully opaque)
+        }, 500); // Duration of the animation in milliseconds (500ms)
+
         $(this).find('.text').addClass('hide');
+        // Finds the element with the class 'text' inside the current 'image-wrapper' element and adds the class 'hide'
     });
 });
